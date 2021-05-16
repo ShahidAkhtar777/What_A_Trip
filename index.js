e73ae0799ee97e3a9f404dd4965139c58522ccda
Shahid Akhtar <shahidakhtar016@gmail.com>
2021-05-16 21:58:44 +0530

    Basic Layout added

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/',function(req,res){
-    res.sendFile(__dirname+ '/index.html');
+    res.sendFile(__dirname+ 'public/index.html');
 });
 
 app.listen(3000,function(){
